7c39f4d723deff1ff2b59b5dc5c50a841d60c780
garveyshah <garveyshah1595@gmail.com>
2025-02-24 14:26:26 +0300

    add temporary contract address

--- src/utils/blockchain.js
+++ src/utils/blockchain.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 import FarmerRegistrationABI from '../../artifacts/contracts/FarmerRegistration.sol/FarmerRegistration.json';
 
-const contractAddress = "YOUR_DEPLOYED_CONTRACT_ADDRESS";
+const contractAddress = "0x7d7491eb3b88d26a1e6c635401cbd97809131053";
 
 export const getContract = async () => {
   const provider = new ethers.BrowserProvider(window.ethereum);
