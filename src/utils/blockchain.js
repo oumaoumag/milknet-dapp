ee0ccb6e9b5827e3e0234728d0584880d6dc6a7c
garveyshah <garveyshah1595@gmail.com>
2025-02-24 16:49:23 +0300

    feat: add function to get contract instance
    - Import  from .
    - Import  for the contract's ABI.
    - Implement  function to create and return a new contract instance using the provided address and signer.

--- src/utils/blockchain.js
+++ src/utils/blockchain.js
@@ -1,10 +1,6 @@
-import { ethers } from 'ethers';
-import FarmerRegistrationABI from '../../artifacts/contracts/FarmerRegistration.sol/FarmerRegistration.json';
+import { Contract } from 'ethers';
+import FarmerRegistrationABI from './FarmerRegistrationABI.json';
 
-const contractAddress = "0x7d7491eb3b88d26a1e6c635401cbd97809131053";
-
-export const getContract = async () => {
-  const provider = new ethers.BrowserProvider(window.ethereum);
-  const signer = await provider.getSigner();
-  return new ethers.Contract(contractAddress, FarmerRegistrationABI.abi, signer);
-};
+export function getContract(address, signer) {
+  return new Contract(address, FarmerRegistrationABI, signer);
+}
