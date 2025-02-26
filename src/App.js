d061fa992e62f6c7b8afe489b4d2a088475457cd
garveyshah <garveyshah1595@gmail.com>
2025-02-26 04:25:28 +0300

    feat: setup main application component
    - Wrap the application in  to provide Web3 context.
    - Use  for routing.
    - Add routes for , , , and  components.
    - Include  component for navigation.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css"
 import { Web3Provider } from './contexts/Web3Context';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header'
+import Landing from './components/Landing';
+import FarmerDashboard from './components/farmers/Dashboard';
+import Marketplace from './components/buyer/Marketplace';
 import FarmerRegistration from './components/farmers/Registration';
-import CreateBatch from './components/batches/CreateBatch';
 
 function App() {
   return (
     <Web3Provider>
-      <div className="App">
-        <h1>MilkNet Marketplace</h1>
-        <FarmerRegistration />
-        <CreateBatch />
-      </div>
+      <BrowserRouter>
+       <Header />
+       <Routes>
+         <Route path="/" element={<Landing />} />
+         <Route path="/farmer" element={<FarmerDashboard/>} />
+         <Route path="/marketplace" element={<Marketplace />} />
+         <Route path="/register-farmer" element={<FarmerRegistration />} />
+       </Routes>
+     </BrowserRouter>
     </Web3Provider>
   );
 }
