726999f06c5294bff3a0f3060fd5301d9753ba9b
garveyshah <garveyshah1595@gmail.com>
2025-02-26 03:22:42 +0300

    feat: add fetch batches function
    - Refactore  to:
      - Retrieve the total number of batches using .
      - Fetch each batch's details using .
      - Return an array of batch details.

--- src/utils/contractCalls.js
+++ src/utils/contractCalls.js
@@ -1,14 +1,12 @@
+// src/utils/contractCalls.js
 export const fetchBatches = async (contract) => {
-    const batchCount = await contract.batchCounter();
-    const batches = [];
-    
-    for (let i = 1; i <= batchCount; i++) {
-      batches.push(await contract.getMilkBatch(i));
-    }
-    
-    return batches;
-  };
+  const batchCount = await contract.batchCounter();
+  const batches = [];
   
-  export const fetchFarmers = async (contract) => {
-    const farmerCount = await contract.farmerCounter();
-  };
+  for (let i = 1; i <= batchCount; i++) {
+    const batch = await contract.getMilkBatch(i);
+    batches.push(batch);
+  }
+  
+  return batches;
+};
