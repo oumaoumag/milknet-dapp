66eb842e2e81a147e9484a9edba3b057fcebdb45
garveyshah <garveyshah1595@gmail.com>
2025-02-28 23:29:29 +0300

    refactor(App): Update routing and component structure
    - Integrated new routes for farmer registration and dashboard.
    - Enhanced application structure with updated component imports.
    - Improved navigation flow and user experience.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+// App.js
 import "./App.css"
 import { Web3Provider } from './contexts/Web3Context';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Landing from './components/Landing';
 import FarmerDashboard from './components/farmers/Dashboard';
 import Marketplace from './components/buyer/Marketplace';
 import FarmerRegistration from './components/farmers/Registration';
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
     <Web3Provider>
       <BrowserRouter>
-       <Header />
-       <Routes>
-         <Route path="/" element={<Landing />} />
-         <Route path="/register-farmer" element={<FarmerRegistration />} />
-         <Route path="/farmer" element={<FarmerDashboard/>} />
-         <Route path="/marketplace" element={<Marketplace />} />
-       </Routes>
-     </BrowserRouter>
+        <Header />
+        <main className="min-h-screen">
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/register-farmer" element={<FarmerRegistration />} />
+            <Route path="/farmer" element={<FarmerDashboard/>} />
+            <Route path="/marketplace" element={<Marketplace />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </Web3Provider>
   );
 }
